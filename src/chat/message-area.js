2ba4b93083f508f05d64ef450405ae51e36620bc
Yoav Prat <yoav.prat@gmail.com>
2019-11-10 12:09:24 +0200

    name to hebrew

--- src/chat/message-area.js
+++ src/chat/message-area.js
@@ -19,7 +19,7 @@ export default class MessageArea extends Component {
             <ol class="chat">
                 {props.messages.map(({name, text, from, time}) => {
                     if (from === 'visitor') {
-                        name = "You";
+                        name = "את/ה";
                     }
                     return (
                         <li class={from}>
