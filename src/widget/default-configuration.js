3d2bb8a5fa9d45cd17543102912e036752a1cf0b
Yoav Prat <yoav.prat@gmail.com>
2019-11-10 11:48:39 +0200

    customizations test

--- src/widget/default-configuration.js
+++ src/widget/default-configuration.js
@@ -1,17 +1,16 @@
 
 export const defaultConfiguration = {
-    titleClosed: 'Click to chat!',
-    titleOpen: 'Let\'s chat!',
+    titleClosed: "הקלקה לצ'אט",
+    titleOpen: 'Workito שרות לקוחות',
     closedStyle: 'chat', // button or chat
     closedChatAvatarUrl: '', // only used if closedStyle is set to 'chat'
     cookieExpiration: 1, // in days. Once opened, closed chat title will be shown as button (when closedStyle is set to 'chat')
-    introMessage: 'Hello! How can we help you?',
-    autoResponse: 'Looking for the first available admin (It might take a minute)',
-    autoNoResponse: 'It seems that no one is available to answer right now. Please tell us how we can ' +
-    'contact you, and we will get back to you as soon as we can.',
-    placeholderText: 'Send a message...',
+    introMessage: 'שלום! איך אפשר לעזור לך?',
+    autoResponse: 'מחפש נציג עבורך (זה יכול לקחת קצת זמן)',
+    autoNoResponse: 'אין כרגע נציג זמין, אפשר להשאיר הודעה ופרטים נוספים ואנחנו נחזור אליכם בהקדם',
+    placeholderText: 'שלח/י הודעה...',
     displayMessageTime: true,
-    mainColor: '#1f8ceb',
+    mainColor: '#296eaf',
     alwaysUseFloatingButton: false,
     desktopHeight: 450,
     desktopWidth: 370
